6dd90ce138dc8a22e4e0237d516be6ad0734ca01
Sergey Kivachuk <you@example.com>
2023-04-16 10:07:47 +0300

    решенная задача 12

---
+++ Task_012/Program.cs
@@ -0,0 +1,29 @@
+﻿// 12. Напишите программу, которая будет принимать на
+// вход два числа и выводить, является ли первое число
+// кратным второму. Если первое число не кратно
+// второму, то программа выводит остаток от деления.
+// 34, 5 -> не кратно, остаток 4
+// 16, 4 -> кратно
+
+int numberA = new Random().Next(1, 99);
+Console.WriteLine($"Случайное первое число  -> {numberA} ");
+int numberB = new Random().Next(1, 99);
+Console.WriteLine($"Случайное второе  число  -> {numberB} "); 
+
+int result = Remainde(numberA, numberB);
+
+if (result == 0 )
+{
+    Console.WriteLine("кратно");
+}
+else
+{
+    Console.WriteLine($"не кратно, остаток {result}");
+}
+
+int Remainde (int numA, int numB) 
+{
+    return numA % numB ; 
+ }
+
+
