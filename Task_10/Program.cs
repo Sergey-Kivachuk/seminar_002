84cd0288f8510ed62a96a87729ac3ed98f5c6f77
Sergey Kivachuk <you@example.com>
2023-04-16 15:11:37 +0300

    решенная задача 10

---
+++ Task_10/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+
+Console.WriteLine("Введите трехзначное число ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number<0)
+number = -number;
+int maxDigit = MaxDigit(number);
+string res = ((number > 100 && number < 999) ? $"готовое число  -> {maxDigit}" : "Вы ввели не трехзначное число");
+Console.Write(res);
+int MaxDigit (int num) 
+{
+    return num / 10 % 10 ;
+ }
+  
