27150ee979ef5f29d4a20dca2904159ff8435ba2
Sergey Kivachuk <you@example.com>
2023-04-16 16:00:44 +0300

    решенная задача 13

---
+++ Task_013/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+
+// 78 -> третьей цифры нет
+
+// 32679 -> 6
+
+int Prompt(string messge)
+{
+    Console.WriteLine(message);
+    int result = Convert.ToInt32(value);
+    return result;
+}
+ int IsSquare(int number) 
+ {
+   while (number > 999);
+   {
+      number /=10;
+   }
+      return number % 10;
+ }      
+
+bool  MaxDigit (int number) 
+{
+    if (number>100)
+    {
+        Console.WriteLine("Третьей цифры нету");
+        return false;
+    }
+    return true;
+}
+int number = Prompt("число ");
+if (MaxDigit (number))
+{
+    Console.WriteLine( IsSquare (number));
+}
