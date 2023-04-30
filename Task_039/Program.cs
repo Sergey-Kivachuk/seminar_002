24e3380199767a1b86eef970c3bc8edc6dddfe77
Sergey Kivachuk <you@example.com>
2023-04-30 17:24:11 +0300

    Решение задачи  39

---
+++ Task_039/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 39: Напишите программу, которая перевернёт
+// одномерный массив (последний элемент будет на первом
+// месте, а первый - на последнем и т.д.)
+// [1 2 3 4 5] -> [5 4 3 2 1]
+// [6 7 3 6] -> [6 3 7 6]
+// 0 1 2 3 4
+// [1 2 3 4 5] -> [5 4 3 2 1]
+//Array.Reverse(array);
+
+void ReverseArray (int[] arr)
+{
+for (int i = 0; i < arr.Length / 2; i++)
+{
+int temp = arr[i];
+arr[i] = arr[arr.Length - 1 - i];
+arr[arr.Length - 1 - i] = temp;
+}
+}
+
+int[] array = CreateArrayRndInt(6, 1, 25);
+PrintArray(array);
+
+Console.WriteLine();
+ReverseArray(array);
+PrintArray(array);
+
+Console.WriteLine();
+Array.Reverse(array);
+PrintArray(array)
