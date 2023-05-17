f53ef4be219a8d2a39e7344be8395bb71460a7a1
Sergey Kivachuk <you@example.com>
2023-05-17 19:12:13 +0300

    Решение задачи  45

--- Task_045/Program.cs
+++ Task_045/Program.cs
@@ -4,12 +4,11 @@
 
 int[] CreateArrayRndInt(int size, int min, int max)
 {
-int[] arr = new int[size];
-Random rnd = new Random();
-
-for (int i = 0; i < arr.Length; i++)
-{
-arr[i] = rnd.Next(min, max + 1);
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+       arr[i] = rnd.Next(min, max + 1);
 }
 return arr;
 }
